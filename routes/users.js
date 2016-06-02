cc7ff4470b9be521b0cf2c3888116e8a29461b64
altynbek.shaidyldaev@gmail.com <altynPc>
2016-06-02 15:07:25 +0600

    User list created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('admin/users', { user: req.user });
+  User.
+      find({}).
+      sort({created: 'desc'}).
+      exec( function (err, users) {
+        if(err) return console.error(err);
+
+        res.render('admin/users', {
+          title: "Список пользователей",
+          users : users,
+          user: req.user
+        })
+      });
 });
 
 module.exports = router;
