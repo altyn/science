8e4aa8da131f45b0e00d1f8d63be8b9fe1d76d3e
altynbek.shaidyldaev@gmail.com <altynPc>
2016-06-29 15:50:59 +0600

    Final versions

--- routes/release.js
+++ routes/release.js
@@ -11,37 +11,6 @@ router.get('/:release/:year', function (req, res) {
     var perPage = 10
         , page = req.params.page > 0 ? req.params.page : 0;
 
-    //var data;
-    //
-    //data = {};
-    //
-    //async.parallel({
-    //    articles: function (callback) {
-    //        return Articles.
-    //                find({}).
-    //                and([{ release: release }, { year: yearrel }]).
-    //                sort({publishedDate: 'desc'}).
-    //                exec( function (err, result) {
-    //                    if(err) return(err);
-    //                        return callback(err, result);
-    //                });
-    //    },
-    //    authors: function (callback) {
-    //        return Article.findOne({_id: id}).select('author.name').sort({publishedDate: 'desc'}).exec( function (err, result) {
-    //            if(err) return(err);
-    //            return callback(err, result);
-    //            console.log(result);
-    //        });
-    //    }
-    //}, function(err, data){
-    //    if(err) return console.error(err);
-    //
-    //    res.render('rel_articles', {
-    //        title: release + ' - ' + yearrel,
-    //        data : data
-    //    })
-    //});
-
     Articles.
         find({}).
         and([{ release: release }, { year: yearrel }]).
@@ -50,7 +19,9 @@ router.get('/:release/:year', function (req, res) {
             if(err) return console.error(err);
                 res.render('rel_articles', {
                     title: release + ' - ' + yearrel,
-                    data : data
+                    data : data,
+                    relese: release,
+                    year_rel: yearrel
                 })
             })
 });
